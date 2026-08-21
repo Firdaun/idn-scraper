76db1da528ba0f6b98d6d5aecf6f624f292493ab
Firdaun <ketuakls776@gmail.com>
2026-08-21 15:00:00 +0700

    update schema dan menampilkan analytics

---
+++ prisma/migrations/20260823120330_remove_unique_view_count/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `uniqueViewCount` on the `LivestreamViewerStat` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "LivestreamViewerStat" DROP COLUMN "uniqueViewCount";
